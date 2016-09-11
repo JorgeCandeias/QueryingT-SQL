e1f69e09b37f515c0e9c92e971a3a72142ec6f24
Jorge Candeias <candeias@fastmail.fm>
2016-09-11 13:17:27 +0100

    Added lab.

---
+++ Notes/Lab2.sql
@@ -0,0 +1,67 @@
+SELECT DISTINCT
+	City,
+	StateProvince
+FROM
+	SalesLT.Address
+;
+
+SELECT TOP 10 PERCENT
+	Name,
+	Weight
+FROM
+	SalesLT.Product
+ORDER BY
+	Weight DESC
+;
+
+SELECT
+	Name,
+	Weight
+FROM
+	SalesLT.Product
+ORDER BY
+	Weight DESC
+OFFSET 10 ROWS
+FETCH NEXT 100 ROWS ONLY
+;
+
+SELECT
+	Name,
+	Color,
+	Size
+FROM
+	SalesLT.Product
+WHERE
+	ProductModelID = 1
+;
+
+SELECT
+	ProductNumber,
+	Name
+FROM
+	SalesLT.Product
+WHERE
+	Color IN ('Black','Red','White')
+	AND Size IN ('S', 'M')
+;
+
+SELECT
+	ProductNumber,
+	Name,
+	ListPrice
+FROM
+	SalesLT.Product
+WHERE
+	ProductNumber LIKE 'BK-%'
+;
+
+SELECT
+	ProductNumber,
+	Name,
+	ListPrice
+FROM
+	SalesLT.Product
+WHERE
+	ProductNumber LIKE 'BK-[^R]%-[0-9][0-9]'
+;
+
